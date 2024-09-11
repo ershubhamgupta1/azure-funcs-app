ea586539731613a3b6d2e0c7e800cc49c6322744
shubham gupta <shubhamgupta@shubhams-MacBook-Air.local>
2024-09-11 19:44:32 +0530

    initial commit

---
+++ src/functions/getHttpTrigger.js
@@ -0,0 +1,15 @@
+const { app } = require('@azure/functions');
+const { app } = require('@azure/functions');
+
+
+app.http('getHttpTrigger', {
+    methods: ['GET', 'POST'],
+    authLevel: 'anonymous',
+    handler: async (request, context) => {
+        context.log(`Http function processed request for url "${request.url}"`);
+
+        const name = request.query.get('name') || await request.text() || 'world';
+
+        return { body: `Hello, ${name}!` };
+    }
+});
